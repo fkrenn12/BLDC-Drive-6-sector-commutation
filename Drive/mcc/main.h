c53296f1235bf917fb69e76acad5b94fab541d2e
fkrenn12 <fkrenn@nanet.at>
2026-02-17 13:33:42 +0100

    Disable SMART_POWERLAB and enable MANUAL_CONTROL mode.
    Commented out the SMART_POWERLAB_HARDWARE definition and switched from FLETUINO_APPLICATION_DEMO to FLETUINO_MANUAL_CONTROL. These changes configure the code for manual control and prepare it to operate without the SMART_POWERLAB hardware.

--- Drive/mcc/main.h
+++ Drive/mcc/main.h
@@ -9,19 +9,19 @@
 #define FCY 100000000UL  //100Mhz
 #define MYADDRESS "#3"              // define the address hardcoded - uncomment if using the hardware with SW400
 
-#define SMART_POWERLAB_HARDWARE     // comment if using the real hardware drive
+// #define SMART_POWERLAB_HARDWARE     // comment if using the real hardware drive
 
 #define CURRENT_CONTROL 1           // 0 = manual control, 1 = automatic control
 #define COMMUTATE 1                 // 0 = no commutation, 1 = commutation
 #define STATEMACHINE 1              // 0 = no state machine, 1 = state machine
 
 
-//#define DEBUG                       // only one option (DEBUG or any FLETUINO) is allowed
+// #define DEBUG                       // only one option (DEBUG or any FLETUINO) is allowed
 // #define DEBUG_SERIAL_COMMAND_HANDLING 
 
-#define FLETUINO_APPLICATION_DEMO
-//#define FLETUINO_PI_CONTROLLER_SETTINGS
-// #define FLETUINO_MANUAL_CONTROL
+// #define FLETUINO_APPLICATION_DEMO
+// #define FLETUINO_PI_CONTROLLER_SETTINGS
+#define FLETUINO_MANUAL_CONTROL
 
 
 // NO NEED TO CHANGE ANYTHING BELOW THIS LINE
