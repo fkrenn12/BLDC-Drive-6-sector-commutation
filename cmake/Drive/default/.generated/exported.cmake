458884463f00c41cc4ebb2d2b24b5c5e59ec085f
fkrenn12 <fkrenn@nanet.at>
2026-04-01 13:54:38 +0200

    Update dsPIC33CK-MC_DFP version to 1.10.386
    Updated all references of Microchip dsPIC33CK-MC_DFP from version 1.8.299 to 1.10.386 across multiple files. This ensures compatibility with the latest pack version and improves build consistency.

--- cmake/Drive/default/.generated/exported.cmake
+++ cmake/Drive/default/.generated/exported.cmake
@@ -4,7 +4,7 @@ set(DEPENDENT_TARGET_DIRDrive_default_DYIqDzYj "${CMAKE_CURRENT_LIST_DIR}/../../
 set(DEPENDENT_BYPRODUCTSDrive_default_DYIqDzYj ${DEPENDENT_TARGET_DIRDrive_default_DYIqDzYj}/${sourceFileNameDrive_default_DYIqDzYj}.s)
 add_custom_command(
     OUTPUT ${DEPENDENT_TARGET_DIRDrive_default_DYIqDzYj}/${sourceFileNameDrive_default_DYIqDzYj}.s
-    COMMAND ${DEPENDENT_MP_BIN2HEXDrive_default_DYIqDzYj} ${DEPENDENT_DEPENDENT_TARGET_ELFDrive_default_DYIqDzYj} --image ${sourceFileNameDrive_default_DYIqDzYj} ${addressDrive_default_DYIqDzYj} ${modeDrive_default_DYIqDzYj} -mdfp=C:/Users/fkren/.mchp_packs/Microchip/dsPIC33CK-MC_DFP/1.8.299/xc16 
+    COMMAND ${DEPENDENT_MP_BIN2HEXDrive_default_DYIqDzYj} ${DEPENDENT_DEPENDENT_TARGET_ELFDrive_default_DYIqDzYj} --image ${sourceFileNameDrive_default_DYIqDzYj} ${addressDrive_default_DYIqDzYj} ${modeDrive_default_DYIqDzYj} -mdfp=C:/Users/fkren/.mchp_packs/Microchip/dsPIC33CK-MC_DFP/1.10.386/xc16 
     WORKING_DIRECTORY ${DEPENDENT_TARGET_DIRDrive_default_DYIqDzYj}
     DEPENDS ${DEPENDENT_DEPENDENT_TARGET_ELFDrive_default_DYIqDzYj})
 add_custom_target(
--- cmake/Drive/default/.generated/rule.cmake
+++ cmake/Drive/default/.generated/rule.cmake
@@ -7,7 +7,7 @@ function(Drive_default_default_XC16_assemble_rule target)
         "-g"
         "-mcpu=33CK64MC105"
         "-Wa,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-g,--no-relax"
-        "-mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.8.299/xc16")
+        "-mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.10.386/xc16")
     list(REMOVE_ITEM options "")
     target_compile_options(${target} PRIVATE "${options}")
     target_compile_definitions(${target}
@@ -21,7 +21,7 @@ function(Drive_default_default_XC16_assemblePreproc_rule target)
         "-g"
         "-mcpu=33CK64MC105"
         "-Wa,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-g,--no-relax"
-        "-mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.8.299/xc16")
+        "-mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.10.386/xc16")
     list(REMOVE_ITEM options "")
     target_compile_options(${target} PRIVATE "${options}")
     target_compile_definitions(${target}
@@ -38,7 +38,7 @@ function(Drive_default_default_XC16_compile_rule target)
         "-msmart-io=1"
         "-Wall"
         "-msfr-warn=off"
-        "-mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.8.299/xc16")
+        "-mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.10.386/xc16")
     list(REMOVE_ITEM options "")
     target_compile_options(${target} PRIVATE "${options}")
     target_compile_definitions(${target}
@@ -49,7 +49,7 @@ function(Drive_default_dependentObject_rule target)
     set(options
         "-c"
         "-mcpu=33CK64MC105"
-        "-mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.8.299/xc16")
+        "-mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.10.386/xc16")
     list(REMOVE_ITEM options "")
     target_compile_options(${target} PRIVATE "${options}")
 endfunction()
@@ -65,7 +65,7 @@ function(Drive_default_link_rule target)
         "-mreserve=data@0x1024:0x1027"
         "-mreserve=data@0x1028:0x104f"
         "-Wl,--script=p33CK64MC105.gld,--local-stack,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D__DEBUG=__DEBUG,--heap=0,--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,--report-mem,--memorysummary,memoryfile.xml"
-        "-mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.8.299/xc16")
+        "-mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.10.386/xc16")
     list(REMOVE_ITEM options "")
     target_link_options(${target} PRIVATE "${options}")
     target_compile_definitions(${target}
@@ -75,7 +75,7 @@ endfunction()
 function(Drive_default_bin2hex_rule target)
     add_custom_target(
         Drive_default_Bin2Hex ALL
-        COMMAND ${MP_BIN2HEX} ${Drive_default_image_name} -a -mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.8.299/xc16
+        COMMAND ${MP_BIN2HEX} ${Drive_default_image_name} -a -mdfp=${PACK_REPO_PATH}/Microchip/dsPIC33CK-MC_DFP/1.10.386/xc16
         WORKING_DIRECTORY ${Drive_default_output_dir}
         BYPRODUCTS "${Drive_default_output_dir}/${Drive_default_image_base_name}.hex"
         COMMENT "Convert build file to .hex")
